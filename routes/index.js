c77418485fe187476a25d9e033d0086f0c439814
nevz <projectnevz@gmail.com>
2022-06-15 21:30:28 +0800

    finish project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hello',
+    user: 'John',
+    added: new Date()
+  },
+  {
+    text: 'Hello world',
+    user: 'Alex',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.locals.title = 'Mini Messageboard';
+  res.locals.messages = messages;
+  res.render('index')
 });
+
+router.get('/new', (req, res) => {
+  res.render('form')
+})
+
+router.post('/new', function(req, res) {
+  const { messageText, name} = req.body;
+
+  messages.push({text: messageText, user: name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
